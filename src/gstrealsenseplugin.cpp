834c64d9ac6a193a735eb3aac9d7141dee287258
Tim Connelly <timpconnelly@gmail.com>
2020-02-18 17:39:02 -0500

    clean up unused vars and set/get caps

--- src/gstrealsenseplugin.cpp
+++ src/gstrealsenseplugin.cpp
@@ -109,6 +109,8 @@ static GstFlowReturn gst_realsense_src_create (GstPushSrc * src, GstBuffer ** bu
 
 static gboolean gst_realsense_src_start (GstBaseSrc * basesrc);
 static gboolean gst_realsense_src_stop (GstBaseSrc * basesrc);
+static GstCaps *gst_realsense_src_get_caps (GstBaseSrc * src, GstCaps * filter);
+static gboolean gst_realsense_src_set_caps (GstBaseSrc * src, GstCaps * caps);
 
 /* GObject vmethod implementations */
 
@@ -139,7 +141,8 @@ gst_realsense_src_class_init (GstRealsenseSrcClass * klass)
   //     gst_static_pad_template_get (&src_factory));
   gst_element_class_add_static_pad_template (gstelement_class, &src_factory);
 
-  // gstbasesrc_class->set_caps = gst_video_test_src_setcaps;
+  gstbasesrc_class->get_caps = gst_realsense_src_get_caps;
+  gstbasesrc_class->set_caps = gst_realsense_src_set_caps;
   // gstbasesrc_class->fixate = gst_video_test_src_src_fixate;
   // gstbasesrc_class->is_seekable = gst_video_test_src_is_seekable;
   // gstbasesrc_class->do_seek = gst_video_test_src_do_seek;
@@ -211,15 +214,15 @@ gst_realsense_src_get_property (GObject * object, guint prop_id, GValue * value,
 }
 
 static GstBuffer *
-gst_realsense_src_create_buffer_from_frameset (GstRealsenseSrc * src, rs2::frameset& frame)
+gst_realsense_src_create_buffer_from_frameset (GstRealsenseSrc * src, rs2::frameset& frame_set)
 {
   GstMapInfo minfo;
   GstBuffer *buf;
 
-  auto vf = frame.as<rs2::video_frame>();
+  auto cframe = frame_set.get_color_frame();
 
   /* TODO: use allocator or use from pool */
-  buf = gst_buffer_new_and_alloc (vf.get_height() * src->gst_stride);
+  buf = gst_buffer_new_and_alloc (cframe.get_height() * src->gst_stride);
 
   /* Copy image to buffer from surface */
   gst_buffer_map (buf, &minfo, GST_MAP_WRITE);
@@ -232,11 +235,11 @@ gst_realsense_src_create_buffer_from_frameset (GstRealsenseSrc * src, rs2::frame
   //     circ_handle->HiResTimeStamp.hour, circ_handle->HiResTimeStamp.min,
   //     circ_handle->HiResTimeStamp.sec, circ_handle->HiResTimeStamp.usec);
 
-  auto rs_stride = vf.get_stride_in_bytes();
+  auto rs_stride = cframe.get_stride_in_bytes();
   /* TODO: use orc_memcpy */
   if (src->gst_stride == rs_stride) 
   {
-    memcpy (minfo.data, ((guint8 *) vf.get_data()), minfo.size);
+    memcpy (minfo.data, ((guint8 *) cframe.get_data()), minfo.size);
   } 
   else 
   {
@@ -245,7 +248,7 @@ gst_realsense_src_create_buffer_from_frameset (GstRealsenseSrc * src, rs2::frame
     for (i = 0; i < src->height; i++) 
     {
       memcpy (minfo.data + i * src->gst_stride,
-          ((guint8 *) vf.get_data()) +
+          ((guint8 *) cframe.get_data()) +
           i * rs_stride, rs_stride);
     }
   }
@@ -272,17 +275,15 @@ gst_realsense_src_create (GstPushSrc * psrc, GstBuffer ** buf)
     // clock_time = gst_clock_get_time (clock);
     // gst_object_unref (clock);
 
-    /* create GstBuffer then release circ buffer back to acquisition */
+    /* create GstBuffer then release */
     *buf = gst_realsense_src_create_buffer_from_frameset(src, frame);
   }
   catch (rs2::error & e)
   {
-    GST_ERROR_OBJECT (
-             src,
-             "Realsense error calling %s (%s)",
-             e.get_failed_function().c_str(),
-             e.get_failed_args().c_str());
-      return GST_FLOW_ERROR;
+    GST_ELEMENT_ERROR (src, RESOURCE, FAILED, 
+        ("RealSense error calling %s (%s)", e.get_failed_function().c_str(), e.get_failed_args().c_str()),
+        (NULL));
+    return GST_FLOW_ERROR;
   }
 
   /* TODO: set timestamps */
@@ -309,8 +310,6 @@ static gboolean
 gst_realsense_src_start (GstBaseSrc * basesrc)
 {
   auto *src = GST_REALSENSESRC (basesrc);
-  GstVideoInfo vinfo;
-
   // GST_OBJECT_LOCK (src);
 
   try 
@@ -334,14 +333,11 @@ gst_realsense_src_start (GstBaseSrc * basesrc)
       // src->accum_frames = 0;
       // src->accum_rtime = 0;
 
-      gst_video_info_init(&src->info);
-
       src->rs_pipeline->start();
       GST_LOG_OBJECT(src, "RealSense pipeline started");
 
       // TODO need to set up format here
       auto frame_set = src->rs_pipeline->wait_for_frames();
-      // auto vf = frame.as<rs2::video_frame>();
       auto cframe = frame_set.get_color_frame();
       auto height = cframe.get_height();
       // auto height = vf.get_height();
@@ -350,23 +346,42 @@ gst_realsense_src_start (GstBaseSrc * basesrc)
       // rs2_frame_metadata_value
       // auto raw_rs_size = vf.get_frame_metadata(RS2_FRAME_METADATA_RAW_FRAME_SIZE);
 
-      gst_video_info_init (&vinfo);
+      gst_video_info_init(&src->info);
+
       switch(rs_format){
         case RS2_FORMAT_RGB8:
-          gst_video_info_set_format(&vinfo, GST_VIDEO_FORMAT_RGB, width, height);
+          gst_video_info_set_format(&src->info, GST_VIDEO_FORMAT_RGB, width, height);
           break;
         case RS2_FORMAT_BGR8:
+          gst_video_info_set_format(&src->info, GST_VIDEO_FORMAT_BGR, width, height);
+          break;
         case RS2_FORMAT_RGBA8:
+          gst_video_info_set_format(&src->info, GST_VIDEO_FORMAT_RGBA, width, height);
+          break;
         case RS2_FORMAT_BGRA8:
+          gst_video_info_set_format(&src->info, GST_VIDEO_FORMAT_BGRA, width, height);
+          break;
         case RS2_FORMAT_RAW16:
         case RS2_FORMAT_Y16:
+          if (G_BYTE_ORDER == G_LITTLE_ENDIAN) 
+          {
+            gst_video_info_set_format (&src->info, GST_VIDEO_FORMAT_GRAY16_LE, width, height);
+          } 
+          else if (G_BYTE_ORDER == G_BIG_ENDIAN) 
+          {
+            gst_video_info_set_format (&src->info, GST_VIDEO_FORMAT_GRAY16_BE, width, height);
+          }
+          break;
         case RS2_FORMAT_YUYV:
+          // FIXME Not exact format match
+          gst_video_info_set_format(&src->info, GST_VIDEO_FORMAT_YVYU, width, height);
+          break;
         default:
-          gst_video_info_set_format(&vinfo, GST_VIDEO_FORMAT_UNKNOWN, width, height);
+          gst_video_info_set_format(&src->info, GST_VIDEO_FORMAT_UNKNOWN, width, height);
           GST_ELEMENT_ERROR (src, RESOURCE, FAILED, ("Unhandled RealSense format %d", rs_format), (NULL));
           // GST_LOG_OBJECT(src, "Unhandled RealSense format %d", rs_format);
       }
-      src->caps = gst_video_info_to_caps (&vinfo);
+      src->caps = gst_video_info_to_caps (&src->info);
   }
   catch (rs2::error & e)
   {
@@ -417,6 +432,56 @@ gst_realsense_src_stop (GstBaseSrc * basesrc)
   // src->lines = NULL;
 
   return TRUE;
+}
+
+static GstCaps *
+gst_realsense_src_get_caps (GstBaseSrc * bsrc, GstCaps * filter)
+{
+  GstRealsenseSrc *src = GST_REALSENSESRC (bsrc);
+  GstCaps *caps;
+
+  if (src->rs_pipeline == nullptr) {
+    caps = gst_pad_get_pad_template_caps (GST_BASE_SRC_PAD (src));
+  } else {
+    caps = gst_caps_copy (src->caps);
+  }
+
+  GST_DEBUG_OBJECT (src, "The caps before filtering are %" GST_PTR_FORMAT,
+      caps);
+
+  if (filter && caps) {
+    GstCaps *tmp = gst_caps_intersect (caps, filter);
+    gst_caps_unref (caps);
+    caps = tmp;
+  }
+
+  GST_DEBUG_OBJECT (src, "The caps after filtering are %" GST_PTR_FORMAT, caps);
+
+  return caps;
+}
+
+static gboolean
+gst_realsense_src_set_caps (GstBaseSrc * bsrc, GstCaps * caps)
+{
+  GstRealsenseSrc *src = GST_REALSENSESRC (bsrc);
+  GstVideoInfo vinfo;
+  // GstStructure *s = gst_caps_get_structure (caps, 0);
+
+  GST_DEBUG_OBJECT (src, "The caps being set are %" GST_PTR_FORMAT, caps);
+
+  gst_video_info_from_caps (&vinfo, caps);
+
+  if (GST_VIDEO_INFO_FORMAT (&vinfo) != GST_VIDEO_FORMAT_UNKNOWN) {
+    src->gst_stride = GST_VIDEO_INFO_COMP_STRIDE (&vinfo, 0);
+  } else {
+    goto unsupported_caps;
+  }
+
+  return TRUE;
+
+unsupported_caps:
+  GST_ERROR_OBJECT (src, "Unsupported caps: %" GST_PTR_FORMAT, caps);
+  return FALSE;
 }
 
 /* entry point to initialize the plug-in
