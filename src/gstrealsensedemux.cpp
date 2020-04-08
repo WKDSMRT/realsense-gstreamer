4ecc2bb0f52dc195aded242e1fe91e1807d35c3d
Tim Connelly <timpconnelly@gmail.com>
2020-04-08 16:52:03 -0400

    add license header to source files

--- src/gstrealsensedemux.cpp
+++ src/gstrealsensedemux.cpp
@@ -1,5 +1,21 @@
-/*
- * TODO add license
+/* GStreamer RealSense is a set of plugins to acquire frames from 
+ * Intel RealSense cameras into GStreamer pipeline.
+ * Copyright (C) <2020> Tim Connelly/WKD.SMRT <timpconnelly@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
  */
 
 /* GStreamer realsense demux
